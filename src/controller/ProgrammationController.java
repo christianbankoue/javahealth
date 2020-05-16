2b07d34127d7e6203cf2df1360518333769059b5
christianbankoue <48108223+christianbankoue@users.noreply.github.com>
2020-05-16 04:01:23 +0300

    Table view pour la programmation et menu medecin

--- src/controller/ProgrammationController.java
+++ src/controller/ProgrammationController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.Programmation;
@@ -17,17 +18,42 @@ import java.util.ResourceBundle;
 
 public class ProgrammationController  implements Initializable {
 
+
     Utilisateur utilisateur;
 
     @FXML
     private TextField nametxt;
     @FXML
     private TextField prenomtxt;
+    @FXML
+    private TextField hopitalSelected;
+    @FXML
+    private TextField domaineMedicalselected;
+    @FXML
+    private TextField medecinSelected;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         nametxt.setText("...");
         prenomtxt.setText("...");
+    }
+
+    public void hopitalun(ActionEvent event){
+        hopitalSelected.setText("hopital 1");
+    }
+
+    public void hopitaldeux(ActionEvent event){
+        hopitalSelected.setText("hopital 2");
+    }
+
+    public void domaineMedical(ActionEvent event){
+        MenuItem mn = (MenuItem)event.getSource();
+        domaineMedicalselected.setText(mn.getText());
+    }
+    public void medecinSelect(ActionEvent event) {
+        MenuItem menu = (MenuItem)event.getSource();
+        medecinSelected.setText(menu.getText());
+
     }
 
     public void injectUtilisateur(Utilisateur utilisateur){
@@ -45,7 +71,7 @@ public class ProgrammationController  implements Initializable {
         programmation.setPrenomPatient(utilisateur.getPrenom());
 
         //--//
-        programmation.setDomaineMedical(DomaineMedical.CONTROLE_DE_ROUTINE.name());
+        programmation.setDomaineMedical(DomaineMedical.valueOf(domaineMedicalselected.getText()).name());
         programmation.setDate( LocalDate.now());
 
         //affichage des 2 du bas a l action du select sur les 2 du haut
@@ -60,4 +86,8 @@ public class ProgrammationController  implements Initializable {
         ((Stage)((Node)event.getSource()).getScene().getWindow()).close();
 
     }
+
+
+
+
 }
